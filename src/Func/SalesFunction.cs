e6e6c39624e01b93f5b7dd37a601ece53c25db2f
Janne Mattila <janne.mattila@hotmail.fi>
2020-04-01 13:27:59 +0300

    Extract API endpoints to own methods

--- src/Func/SalesFunction.cs
+++ src/Func/SalesFunction.cs
@@ -1,4 +1,6 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
@@ -7,22 +9,52 @@ using Microsoft.Extensions.Logging;
 
 namespace Func
 {
-    public static class SalesFunction
+    public class SalesFunction
     {
+        private readonly ISecurityValidator _securityValidator;
+
+        public SalesFunction(
+            ISecurityValidator securityValidator)
+        {
+            _securityValidator = securityValidator;
+        }
+
         [FunctionName("Sales")]
-        public static IActionResult Run(
+        public async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", "delete", Route = "sales/{id?}")] HttpRequest req,
             string id,
             ILogger log)
         {
             log.LogInformation("Sales function processing request.");
+
+            var principal = await _securityValidator.GetClaimsPrincipalAsync(req, log);
+            if (principal == null)
+            {
+                return new UnauthorizedResult();
+            }
+
             return req.Method switch
             {
-                "GET" => new OkObjectResult($"sales data fetch {id}"),
-                "POST" => new OkObjectResult($"sales data updated {id}"),
-                "DELETE" => new OkResult(),
-                _ => new StatusCodeResult((int)HttpStatusCode.NotImplemented),
+                "GET" => Get(req, id),
+                "POST" => Post(req, id),
+                "DELETE" => Delete(req, id),
+                _ => new StatusCodeResult((int)HttpStatusCode.NotImplemented)
             };
+        }
+
+        private static IActionResult Get(HttpRequest req, string id)
+        {
+            return new OkObjectResult($"sales data fetch {id}");
+        }
+
+        private static IActionResult Post(HttpRequest req, string id)
+        {
+            return new OkObjectResult($"sales data updated {id}");
+        }
+
+        private static IActionResult Delete(HttpRequest req, string id)
+        {
+            return new OkResult();
         }
     }
 }

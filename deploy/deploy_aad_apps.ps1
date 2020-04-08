2a0bc27b8d204c9fabd6a7f56d1b5f7cdb3f189e
Janne Mattila <janne.mattila@hotmail.fi>
2020-04-08 14:27:24 +0300

    Add AAD automation script for API and Reader apps

--- deploy/deploy_aad_apps.ps1
+++ deploy/deploy_aad_apps.ps1
@@ -1,4 +1,13 @@
 Param (
+    [Parameter(HelpMessage="Deployment environment name")] 
+    [string] $EnvironmentName = "local",
+
+    [Parameter(HelpMessage="SPA Reader address root uri")] 
+    [string] $SPAReaderUri = "https://localhost:44387/",
+    
+    [Parameter(HelpMessage="SPA Writer address root uri")] 
+    [string] $SPAWriterUri = "https://localhost:44388/",
+
     [Parameter(HelpMessage="Deployment target resource group")] 
     [string] $ResourceGroupName = "rg-spafunc-local",
     
@@ -14,38 +23,102 @@ Param (
 
 $ErrorActionPreference = "Stop"
 
-# AzureAD app creation:
-$identifierUri = "https://" + $AzureADAppName.ToLower().Replace(" ", "") + "." + $TenantName
+# Use existing Azure context to login to Azure AD
+$context = Get-AzContext
+$accountId = $context.Account.Id
+$tenant = $context.Tenant.TenantId
+$scope = "https://graph.windows.net" # Azure AD Graph API
+$dialog = [Microsoft.Azure.Commands.Common.Authentication.ShowDialog]::Never
 
-$app = Get-AzADApplication -IdentifierUri  $identifierUri
-$quizApp = $null
+$azureSession = [Microsoft.Azure.Commands.Common.Authentication.AzureSession]::Instance.AuthenticationFactory.Authenticate($context.Account, $context.Environment, $tenant, $null, $dialog, $null, $scope)
 
-if ($app.length -eq 1)
+# Azure AD Graph API token
+$accessToken = $azureSession.AccessToken
+
+$aadInstalledModule = Get-Module -Name "AzureAD" -ListAvailable
+if ($null -eq $aadInstalledModule)
 {
-    $quizApp = $app[0]
+    Install-Module AzureAD -Scope CurrentUser
 }
 else
 {
-    #
-    # Note: "New-AzADApplication" does not yet support
-    # setting up "signInAudience" to "AzureADandPersonalMicrosoftAccount"
-    # 
-    Write-Host "Creating new AAD App: $identifierUri"
-    $aadApp = New-AzADApplication `
-        -AvailableToOtherTenants $true `
-        -DisplayName $AzureADAppName `
-        -ReplyUrls "http://localhost/signin-oidc" `
-        -IdentifierUris $identifierUri
-    
-    New-AzADServicePrincipal -ApplicationId $aadApp.ApplicationId
-    $quizApp = $aadApp
+    Import-Module AzureAD
 }
 
+Connect-AzureAD -AadAccessToken $accessToken -AccountId $accountId -TenantId $tenant
 
-$replyUrls = [array]$quizApp.ReplyUrls
-if ($replyUrls.length -eq 0 -or
-    $replyUrls[0].StartsWith($webAppUri) -eq $false)
-{
-    # Update ReplyUrls to the AzureAD application
-    Set-AzADApplication -ObjectId $quizApp.ObjectId -ReplyUrls "$webAppUri/signin-oidc"
-}
+######################
+# Setup functions app:
+# - Expose API "Sales.Read"
+# - Expose API "Sales.ReadWrite"
+$permissions = New-Object System.Collections.Generic.List[Microsoft.Open.AzureAD.Model.OAuth2Permission]
+
+# Known identifiers of Microsoft Graph API
+$microsoftGraphAPI = "00000003-0000-0000-c000-000000000000"
+$userRead = "e1fe6dd8-ba31-4d61-89e7-88639da4683d" # "User.Read"
+
+# Custom identifiers for our APIs
+$permissionSalesRead = "d2dc4339-3161-4d78-a579-8b50f4c4da39" # "Sales.Read"
+$permissionSalesReadWrite = "39579f07-da63-4735-850d-f802b0d08057" # "Sales.ReadWrite"
+
+$readPermission = New-Object Microsoft.Open.AzureAD.Model.OAuth2Permission
+$readPermission.Id = $permissionSalesRead
+$readPermission.Value = "Sales.Read"
+$readPermission.Type = "User"
+$readPermission.AdminConsentDisplayName = "Admin consent for granting read access to sales data"
+$readPermission.AdminConsentDescription = "Admin consent for granting read access to sales data"
+$readPermission.UserConsentDisplayName = "Read access to sales data"
+$readPermission.UserConsentDescription = "Read access to sales data"
+$permissions.Add($readPermission)
+
+$readWritePermission = New-Object Microsoft.Open.AzureAD.Model.OAuth2Permission
+$readWritePermission.Id = $permissionSalesReadWrite
+$readWritePermission.Value = "Sales.ReadWrite"
+$readWritePermission.Type = "User"
+$readWritePermission.AdminConsentDisplayName = "Admin consent for granting read-write access to sales data"
+$readWritePermission.AdminConsentDescription = "Admin consent for granting read-write access to sales data"
+$readWritePermission.UserConsentDisplayName = "Read-write access to sales data"
+$readWritePermission.UserConsentDescription = "Read-write access to sales data"
+$permissions.Add($readWritePermission)
+
+$apiApplication = New-AzureADApplication -DisplayName "SPA-FUNC API $EnvironmentName" `
+    -IdentifierUris "api://spa-func.$EnvironmentName" `
+    -PublicClient $false `
+    -Oauth2Permissions $permissions
+$apiApplication
+
+$spn = New-AzureADServicePrincipal -AppId $apiApplication.AppId
+
+###########################
+# Setup SPASalesReader app:
+$readerAccesses = New-Object System.Collections.Generic.List[Microsoft.Open.AzureAD.Model.RequiredResourceAccess]
+
+# API permission for "User.Read" in Microsoft Graph
+$readerUserRead = New-Object Microsoft.Open.AzureAD.Model.ResourceAccess
+$readerUserRead.Id = $userRead # "User.Read"
+$readerUserRead.Type = "Scope"
+
+$readerGraph = New-Object Microsoft.Open.AzureAD.Model.RequiredResourceAccess
+$readerGraph.ResourceAppId = $microsoftGraphAPI # "Microsoft Graph API"
+$readerGraph.ResourceAccess = New-Object System.Collections.Generic.List[Microsoft.Open.AzureAD.Model.ResourceAccess]
+$readerGraph.ResourceAccess.Add($readerUserRead)
+
+# API permission for "Sales.Read" in SPA-FUNC
+$readerSalesRead = New-Object Microsoft.Open.AzureAD.Model.ResourceAccess
+$readerSalesRead.Id = $readPermission.Id # "Sales.Read"
+$readerSalesRead.Type = "Scope"
+
+$readerApi = New-Object Microsoft.Open.AzureAD.Model.RequiredResourceAccess
+$readerApi.ResourceAppId = $apiApplication.AppId # "SPA FUNC"
+$readerApi.ResourceAccess = New-Object System.Collections.Generic.List[Microsoft.Open.AzureAD.Model.ResourceAccess]
+$readerApi.ResourceAccess.Add($readerSalesRead)
+
+# Add required accesses
+$readerAccesses.Add($readerGraph)
+$readerAccesses.Add($readerApi)
+
+$readerApplication = New-AzureADApplication -DisplayName "SPA-FUNC Sales Reader $EnvironmentName" `
+    -Oauth2AllowImplicitFlow $true `
+    -Homepage $SPAReaderUri `
+    -ReplyUrls $SPAReaderUri `
+    -RequiredResourceAccess $readerAccesses
